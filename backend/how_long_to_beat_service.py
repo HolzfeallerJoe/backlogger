8cbec05c3d3ce47234657a6c4dc85f1e7b0c3c3e
Dominik <renken@ascora.de>
2025-06-15 14:35:39 +0200

    Made the get_est_length function return a prober value

--- backend/how_long_to_beat_service.py
+++ backend/how_long_to_beat_service.py
@@ -4,13 +4,11 @@ from howlongtobeatpy import HowLongToBeat, HowLongToBeatEntry
 
 
 def get_est_length(name: str) -> Optional[int]:
-	ests: List[HowLongToBeatEntry] = HowLongToBeat().search(
+	ests: List[HowLongToBeatEntry] = HowLongToBeat(1).search(
 		name, similarity_case_sensitive=False
 	)
 
 	if ests:
 		for est in ests:
-			# TODO: Add the real est and some logic how to determine what game is meant
-			return 0
-			# return math.ceil(est.main_extra)
+			return None if est.main_extra == 0 else est.main_extra
 	return None
